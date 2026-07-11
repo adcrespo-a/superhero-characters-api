6b83a078e311624938fa0716f2fa184d8c10d569
adcrespo-a <adcrespo@uncg.edu>
2026-07-11 15:58:26 -0400

    commit

---
+++ src/test/java/com/superhero_characters_api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superhero_characters_api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
